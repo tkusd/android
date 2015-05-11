8837053d910e2741a4d1e22e5ae9f5a5d63f1463
MelonH <bramble0290@gmail.com>
2015-05-12 00:04:43 +0800

    簡單的登入 先用Intent 連到google

--- app/src/main/java/tw/tkusd/appstudio/MainActivity.java
+++ app/src/main/java/tw/tkusd/appstudio/MainActivity.java
@@ -1,17 +1,33 @@
 package tw.tkusd.appstudio;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEdtID,mEdtPassword;
+    private Button mBtnOK;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEdtID=(EditText)findViewById(R.id.edtID);
+        mEdtPassword=(EditText)findViewById(R.id.edtPassword);
+        mBtnOK=(Button)findViewById(R.id.btnOK);
+
+        mBtnOK.setOnClickListener(btnOKOnClick);
     }
 
 
@@ -21,7 +37,14 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
+    private View.OnClickListener btnOKOnClick=new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            Uri uri= Uri.parse("http://www.google.com");
+            Intent it=new Intent(Intent.ACTION_VIEW,uri);
+            startActivity(it);
+        }
+    };
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
