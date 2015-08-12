3ca224476ff3d6f37de37dca42a57a28ec608117
chouwenlin <a87740467@yahoo.com.tw>
2015-08-12 20:31:49 +0800

    delete icon

--- app/src/main/java/tw/tkusd/appstudio/app/ProjectListActivity.java
+++ app/src/main/java/tw/tkusd/appstudio/app/ProjectListActivity.java
@@ -74,7 +74,7 @@ public class ProjectListActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_setting) {
             return true;
         }
         return super.onOptionsItemSelected(item);
