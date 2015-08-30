0b517d55657a8bffb52409f2e265a3dc7424558b
chouwenlin <a87740467@yahoo.com.tw>
2015-08-30 15:33:34 +0800

    add intent finsih

--- app/src/main/java/tw/tkusd/appstudio/app/LoginActivity.java
+++ app/src/main/java/tw/tkusd/appstudio/app/LoginActivity.java
@@ -69,6 +69,7 @@ public class LoginActivity extends AppCompatActivity{
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
